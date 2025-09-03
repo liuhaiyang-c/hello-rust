a6987cc5471ef99e846eca3032920b08fc919412
liu haiyang <lhy.hy@outlook.com>
2025-09-03 22:10:08 +0800

    初始化 Rust 项目，添加基本文件结构和示例代码

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hello Rust
+
+## 基础语法
+
+使用 `let` 声明变量，只能被赋值/初始化一次。
+
+```rust
+let number:u8;
+number = 1; // correct
+number = 2; // error
+```
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let welcome = String::from("猜数游戏");
+    println!("{}", welcome);
+
+    let number: u8;
+    number = 1;
+    //ERROR number = 2;
+    println!("number = {}", number);
+}
