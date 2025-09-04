e46f76358594b09b598a9bb1cfea1250c5491bb9
刘海阳 <hilariouhiss@outlook.com>
2025-09-04 19:16:07 +0800

    模块化

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,5 @@
-use std::cmp::Ordering;
-
-use rand::Rng;
+mod guessing_game;
 
 fn main() {
-    let welcome = String::from("Guess the number!");
-    println!("{}", welcome);
-    println!("Please input your guess");
-
-    let the_num = get_random_num();
-
-    loop {
-        let guess = get_user_input();
-
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please input a Number!");
-                continue;
-            }
-        };
-
-        match guess.cmp(&the_num) {
-            Ordering::Equal => {
-                println!("YOU WIN!");
-                break;
-            }
-            Ordering::Greater => println!("your guess is greater"),
-            Ordering::Less => println!("your guess is smaller"),
-        }
-    }
-}
-
-fn get_user_input() -> String {
-    let mut input = String::new();
-    match std::io::stdin().read_line(&mut input) {
-        Ok(_) => input,
-        Err(_) => String::new(),
-    }
-}
-
-fn get_random_num() -> i32 {
-    rand::rng().random_range(1..=100) // 1..100 左闭右开
+    guessing_game::run();
 }
